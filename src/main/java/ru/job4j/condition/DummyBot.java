dadb5725008b090c18e58b0ff77db1db56c824e4
Vasilij Moiseev <vmoiseev@express42.com>
2023-12-22 23:20:38 +0300

    4.1.2. Глупый бот [#31323]

---
+++ src/main/java/ru/job4j/condition/DummyBot.java
@@ -0,0 +1,23 @@
+package ru.job4j.condition;
+
+public class DummyBot {
+
+    public static String answer(String question) {
+        String result = "I don't know. Please, ask another question.";
+        if ("Hi, Bot.".equals(question)) {
+            result = "Hi, SmartAss.";
+        } else if ("Bye.".equals(question)) {
+            result = "See you later.";
+        } else {
+            result = "I don't know. Please, ask another question.";
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        String result = DummyBot.answer("Hi, Bot.");
+        System.out.println(result);
+        result = DummyBot.answer("Bye.");
+        System.out.println(result);
+    }
+}
