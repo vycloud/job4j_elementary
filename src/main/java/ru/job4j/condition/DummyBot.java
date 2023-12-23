69f143c46af140ce0177f5159dce2a2858527dcc
Vasilij Moiseev <vmoiseev@express42.com>
2023-12-23 13:47:03 +0300

    4.1.2. Глупый бот [#31323 #468153]

--- src/main/java/ru/job4j/condition/DummyBot.java
+++ src/main/java/ru/job4j/condition/DummyBot.java
@@ -8,8 +8,6 @@ public class DummyBot {
             result = "Hi, SmartAss.";
         } else if ("Bye.".equals(question)) {
             result = "See you later.";
-        } else {
-            result = "I don't know. Please, ask another question.";
         }
         return result;
     }
