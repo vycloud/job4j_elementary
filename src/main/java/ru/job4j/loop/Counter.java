7f5231c7fbf56e131846fe3efc220d3665de5577
Vasilij Moiseev <vmoiseev@express42.com>
2023-12-24 15:56:10 +0300

    5.0.1. Сумма чисел. [#226416]

---
+++ src/main/java/ru/job4j/loop/Counter.java
@@ -0,0 +1,21 @@
+package ru.job4j.loop;
+
+public class Counter {
+
+    public static int sum(int start, int finish) {
+        int sum = 0;
+        for (int number = start; number <= finish; number++) {
+            sum = sum + number;
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(sum(0, 10));
+        System.out.println(sum(3, 8));
+        System.out.println(sum(1, 1));
+        System.out.println(sum(0, 7));
+        System.out.println(sum(500, 1000));
+        System.out.println(sum(17, 7));
+    }
+}
---
+++ src/test/java/ru/job4j/loop/CounterTest.java
@@ -0,0 +1,43 @@
+package ru.job4j.loop;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class CounterTest {
+
+    @Test
+    void whenFrom0To10Then55() {
+        int start = 0;
+        int finish = 10;
+        int result = Counter.sum(start, finish);
+        int expected = 55;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void whenFrom100To1000Then495550() {
+        int start = 100;
+        int finish = 1000;
+        int result = Counter.sum(start, finish);
+        int expected = 495550;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void whenFrom1To1Then1() {
+        int start = 1;
+        int finish = 1;
+        int result = Counter.sum(start, finish);
+        int expected = 1;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void whenFrom5To3Then0() {
+        int start = 5;
+        int finish = 3;
+        int result = Counter.sum(start, finish);
+        int expected = 0;
+        assertThat(result).isEqualTo(expected);
+    }
+}
